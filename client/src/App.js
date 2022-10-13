0d4dc71754715961c6c6031e2831a6153a091ef9
Fabiana Neves <fabineves_92@hotmail.com>
2022-10-13 23:15:36 +0200

    Insert Leaflet JS and Leaflet React library

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import './App.css'
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <h1>hello map</h1>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={5}
+        scrollWheelZoom={false}
+        style={{ height: '1800px' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+      </MapContainer>
+    </div>
+  )
 }
 
 export default App
