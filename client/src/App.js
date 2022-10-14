9ec9f2a291722794bc630ac2dbd975e9e183c330
Fabiana Neves <fabineves_92@hotmail.com>
2022-10-14 12:08:18 +0200

    insert markers on interactive map

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,36 @@
+//IMPORTS
+
 import './App.css'
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+  Marker,
+  Popup,
+  useMapEvent,
+} from 'react-leaflet'
+import { useCallback } from 'react'
 
 function App() {
   return (
     <div className='App'>
-      <h1>hello map</h1>
+      <h1>POWER PLANTS </h1>
       <MapContainer
         center={[51.505, -0.09]}
-        zoom={5}
+        zoom={13}
         scrollWheelZoom={false}
-        style={{ height: '1800px' }}
+        style={{ height: '1400px', width: '70%' }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
       </MapContainer>
     </div>
   )
